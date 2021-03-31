2eec99283878577fe26e389818e239502f2933ef
Will Jenner <willjenner29@gmail.com>
2021-03-31 19:16:36 +0100

    more changes

--- MLTraining/Program.cs
+++ MLTraining/Program.cs
@@ -15,20 +15,20 @@ namespace MLTraining
         
         private static void Main(string[] args)
         {
-            //LowFrequencyTrainer lft = new LowFrequencyTrainer();
-            //MediumFrequencyTrainer mft = new MediumFrequencyTrainer();
-            //HighFrequencyTrainer hft = new HighFrequencyTrainer();
+            LowFrequencyTrainer lft = new LowFrequencyTrainer();
+            MediumFrequencyTrainer mft = new MediumFrequencyTrainer();
+            HighFrequencyTrainer hft = new HighFrequencyTrainer();
 
             //AllBandTrainer abt = new AllBandTrainer();
 
-            FloorLowFrequencyTrainer flft = new FloorLowFrequencyTrainer();
-            CeilingLowFrequencyTrainer clft = new CeilingLowFrequencyTrainer();
+            //FloorLowFrequencyTrainer flft = new FloorLowFrequencyTrainer();
+            //CeilingLowFrequencyTrainer clft = new CeilingLowFrequencyTrainer();
 
-            FloorMediumFrequencyTrainer fmft = new FloorMediumFrequencyTrainer();
-            CeilingMediumFrequencyTrainer cmft = new CeilingMediumFrequencyTrainer();
+            //FloorMediumFrequencyTrainer fmft = new FloorMediumFrequencyTrainer();
+            //CeilingMediumFrequencyTrainer cmft = new CeilingMediumFrequencyTrainer();
 
-            FloorHighFrequencyTrainer fhft = new FloorHighFrequencyTrainer();
-            CeilingHighFrequencyTrainer chft = new CeilingHighFrequencyTrainer();
+            //FloorHighFrequencyTrainer fhft = new FloorHighFrequencyTrainer();
+            //CeilingHighFrequencyTrainer chft = new CeilingHighFrequencyTrainer();
 
 
             Console.WriteLine("=============== End of process, hit any key to finish ===============");
